ec4d07aba0f6ca506c042718d7548fafd962a817
7osain <h.k.h.e.2000@gmail.com>
2024-10-11 13:29:17 +0300

    Schedule the report generator

--- routes/console.php
+++ routes/console.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+Schedule::command('report:generate')->weeklyOn(6, '1:00');
