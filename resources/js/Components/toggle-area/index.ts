5bf04cf380e4f85c20c19889bc6c085c0fdd8074
7osain <h.k.h.e.2000@gmail.com>
2024-10-14 10:24:32 +0300

    Toggle area todo

---
+++ resources/js/Components/toggle-area/index.ts
@@ -0,0 +1,3 @@
+export { default as ToggleArea } from './ToggleArea.vue'
+export { default as ToggleAreaTrigger } from './ToggleAreaTrigger.vue'
+export { default as ToggleAreaSheet } from './ToggleAreaSheet.vue'
